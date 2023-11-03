b64bd61bb06895d05f403ad66e4068679b5fa959
fricklerhandwerk <valentin@fricklerhandwerk.de>
2023-11-03 11:15:34 +0100

    remove superfluous special arg

--- default.nix
+++ default.nix
@@ -2,15 +2,14 @@
 let
   pkgs = import inputs.nixpkgs { config = { }; overlays = [ ]; };
   pkgs-unstable = import inputs.nixpkgs-unstable { config = { }; overlays = [ ]; };
-  eval = modules: pkgs.lib.evalModules {
-    class = "htmnix";
-    specialArgs = { inherit (pkgs) lib; };
-    modules = modules ++ [ (import ./modules/document.nix) ];
-  };
   nix-unit = (pkgs-unstable.callPackage inputs.nix-unit {
     srcDir = inputs.nix-unit;
     nix = pkgs-unstable.nixUnstable;
   });
+  eval = modules: pkgs.lib.evalModules {
+    class = "htmnix";
+    modules = modules ++ [ (import ./modules/document.nix) ];
+  };
 in
 {
   inherit eval;
