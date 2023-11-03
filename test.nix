7eee0a6a81827722c6516c6d0f62bf275abc0ad3
fricklerhandwerk <valentin@fricklerhandwerk.de>
2023-11-03 05:19:34 +0100

    write out expected value as multiline string
    this makes it much easier to read

--- test.nix
+++ test.nix
@@ -15,6 +15,12 @@ in
         site = renderer.eval [ input ];
       in
       renderer.renderDocument site.config.documents.myDocument;
-    expected = "<html>\n<meta>\n  <title>test hello</title>\n</meta>\n</html>\n";
+    expected = ''
+      <html>
+      <meta>
+        <title>test hello</title>
+      </meta>
+      </html>
+    '';
   };
 }
