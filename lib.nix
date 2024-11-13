b126adf80e3ef82c0e442a5d2e1d82fe0a88620f
valentin gagarin <valentin.gagarin@tweag.io>
2024-11-13 15:24:40 +0100

    extract main document conversion

--- lib.nix
+++ lib.nix
@@ -1,17 +1,38 @@
 { pkgs, lib, ... }:
-{
+rec {
+  /**
+    Convert a Nix document to HTML
+  */
+  html = document: name:
+    builtins.toFile "${name}.html" ''
+      <html>
+      <head>
+        <title>${document.title}</title>
+      </head>
+      <body>
+      ${builtins.readFile (commonmark document.body name)}
+      <body>
+      </html>
+    '';
+
+  /**
+    Convert a commonmark string to HTML
+  */
+  commonmark = markdown: name:
+    pkgs.runCommand "${name}.html" { buildInputs = [ pkgs.cmark ]; } ''
+      cmark ${builtins.toFile "${name}.md" markdown} > $out
+    '';
+
   files = dir: lib.mapAttrs'
     (
-      name: value:
+      attrname: value:
         let
-          html = "${lib.removeSuffix ".nix" name}.html";
-          md = "${lib.removeSuffix ".nix" name}.md";
+          document = import (dir + "/${attrname}");
+          name = lib.removeSuffix ".nix" attrname;
         in
         {
-          name = html;
-          value = pkgs.runCommand html { buildInputs = with pkgs; [ cmark ]; } ''
-            cmark ${builtins.toFile md (import (dir + "/${name}")).body} > $out
-          '';
+          name = "${name}.html";
+          value = html document "${name}.html";
         }
     )
     (builtins.readDir dir);
